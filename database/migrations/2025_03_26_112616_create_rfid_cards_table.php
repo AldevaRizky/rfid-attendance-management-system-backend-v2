58b63b8839a38d690cb710559540a0857ad68999
Aldeva Rizky <aldevarwpp@gmail.com>
2025-03-26 18:35:47 +0700

    feat(database): add RFID cards table migration

---
+++ database/migrations/2025_03_26_112616_create_rfid_cards_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rfid_cards', function (Blueprint $table) {
+            $table->id();
+            $table->string('card_number', 50)->unique();
+            $table->enum('status', ['active', 'inactive']);
+            $table->date('issued_date')->nullable();
+            $table->date('expired_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rfid_cards');
+    }
+};
